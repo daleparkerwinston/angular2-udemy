c97c0666e0ccf20c282c3869f5273fcfc223a58e
Dale Winston <dale.winston@aptecllc.com>
2016-10-29 11:22:04 -0400

    First

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
 
 import {AppComponent} from "./app.component";
+import {MessageComponent} from "./messages/message.component";
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule],
+    declarations: [AppComponent, MessageComponent],
+    imports: [BrowserModule, FormsModule],
     bootstrap: [AppComponent]
 })
 export class AppModule {
